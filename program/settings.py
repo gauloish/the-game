4dc35dbe41c56632dc06493b79eebf2fa71f42fd
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-02 15:24:07 -0300

    formatted code

--- program/settings.py
+++ program/settings.py
@@ -1,8 +1,8 @@
-class Settings():
+class Settings:
     def __init__(self):
         self.width = 1200
         self.height = 800
 
         self.back = [230, 230, 230]
 
-        self.display = 'The Game'
+        self.display = "The Game"
