09aa677ff601c566003ab3ba6b4477651c661c5f
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-02 15:04:35 -0300

    changed program/ship.py images default path to linux path style

--- program/ship.py
+++ program/ship.py
@@ -4,13 +4,16 @@ from .functions import getPath
 from .life import Life
 from .bullets import Bullet
 
-cores = {0: getPath('images\ships\ship_red.png'),
-         1: getPath('images\ships\ship_pink.png'),
-         2: getPath('images\ships\ship_purple.png'),
-         3: getPath('images\ships\ship_blue.png'),
-         4: getPath('images\ships\ship_green.png')}
+cores = {
+    0: getPath("images/ships/ship_red.png"),
+    1: getPath("images/ships/ship_pink.png"),
+    2: getPath("images/ships/ship_purple.png"),
+    3: getPath("images/ships/ship_blue.png"),
+    4: getPath("images/ships/ship_green.png"),
+}
 
-class Ship():
+
+class Ship:
     def __init__(self, screen, color, vel, life):
         self.screen = screen
         self.color = color
@@ -29,35 +32,43 @@ class Ship():
         self.life = life
 
         if self.life == 1:
-            self.lifes = [Life(screen, self.rect.centerx + 30*n, 780) for n in range(0, life)]
+            self.lifes = [
+                Life(screen, self.rect.centerx + 30 * n, 780) for n in range(0, life)
+            ]
         elif self.life == 3:
-            self.lifes = [Life(screen, self.rect.centerx - 29 + 30*n, 780) for n in range(0, life)]
+            self.lifes = [
+                Life(screen, self.rect.centerx - 29 + 30 * n, 780)
+                for n in range(0, life)
+            ]
         elif self.life == 5:
-            self.lifes = [Life(screen, self.rect.centerx - 59 + 30*n, 780) for n in range(0, life)]
+            self.lifes = [
+                Life(screen, self.rect.centerx - 59 + 30 * n, 780)
+                for n in range(0, life)
+            ]
 
         self.velocity = vel
 
         self.bullet = Bullet(screen, self.rect.centerx, self.rect.bottom)
-    
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
 
         for life in self.lifes:
             life.blitme()
-    
+
     def update(self):
         if self.movingL and self.rect.centerx >= 35:
             self.rect.centerx -= self.velocity
 
             for life in self.lifes:
                 life.rect.centerx -= self.velocity
-        
+
         if self.movingR and self.rect.centerx <= 1165:
             self.rect.centerx += self.velocity
 
             for life in self.lifes:
                 life.rect.centerx += self.velocity
-    
+
     def hit(self):
         self.life -= 1
 
