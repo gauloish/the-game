410aee8892c94afd4798983ed3f5d06a24c686fd
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-02 14:43:19 -0300

    changed program/bullets.py images default path to linux path style

--- program/bullets.py
+++ program/bullets.py
@@ -2,11 +2,12 @@ import pygame
 
 from .functions import getPath
 
-class Bullet():
+
+class Bullet:
     def __init__(self, screen, center, bottom):
         self.screen = screen
 
-        self.image = pygame.image.load(getPath('images\munition\_bullet.png'))
+        self.image = pygame.image.load(getPath("images/munition/_bullet.png"))
 
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
@@ -17,16 +18,16 @@ class Bullet():
         self.moving = False
 
         self.velocity = 1
-    
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
-    
+
     def update(self, center, bottom):
         if self.moving:
             self.rect.bottom -= self.velocity
         else:
             self.rect.centerx = center
             self.rect.bottom = bottom - 72
-        
+
         if self.rect.bottom <= 0:
             self.moving = False
