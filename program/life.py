c90e98902990fd35f21028d09a91c3f98b4d0452
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-02 16:21:21 -0300

    changed images names

--- program/life.py
+++ program/life.py
@@ -7,7 +7,7 @@ class Life:
     def __init__(self, screen, x, y):
         self.screen = screen
 
-        self.image = pygame.image.load(getPath("images/life/_life.png"))
+        self.image = pygame.image.load(getPath("images/life/life.png"))
 
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
