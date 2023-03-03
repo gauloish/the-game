f00d77345ce18a733213fbbae6d6113706ea111b
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-03 01:43:37 -0300

    added a documentation to classes and methods in ship.py

--- program/ship.py
+++ program/ship.py
@@ -4,7 +4,7 @@ from .functions import getPath
 from .life import Life
 from .bullets import Bullet
 
-cores = {
+colors = {
     0: getPath("images/ships/ship_red.png"),
     1: getPath("images/ships/ship_pink.png"),
     2: getPath("images/ships/ship_purple.png"),
@@ -14,11 +14,37 @@ cores = {
 
 
 class Ship:
-    def __init__(self, screen, color, vel, life):
+    """Ship
+
+    Attributes:
+        screen: pygame.Surface
+        color: int
+        image: pygame.Surface
+        rect: pygame.Rect
+        screen_rect: pygame.Rect
+        centerx: int
+        bottom: int
+        moving_left: bool
+        moving_right: bool
+        life: int
+        velocity: int
+        bullet: Bullet
+    """
+
+    def __init__(self, screen, color, velocity, life):
+        """Initialize ship
+
+        Args:
+            screen (pygame.Surface): game screen
+            color (int): ship color number
+            velocity (int): ship velocity
+            life (int): ship life
+        """
+
         self.screen = screen
         self.color = color
 
-        self.image = pygame.image.load(cores[color])
+        self.image = pygame.image.load(colors[color])
 
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
@@ -26,8 +52,8 @@ class Ship:
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom - 50
 
-        self.movingL = False
-        self.movingR = False
+        self.moving_left = False
+        self.moving_right = False
 
         self.life = life
 
@@ -46,30 +72,36 @@ class Ship:
                 for n in range(0, life)
             ]
 
-        self.velocity = vel
+        self.velocity = velocity
 
         self.bullet = Bullet(screen, self.rect.centerx, self.rect.bottom)
 
     def blitme(self):
+        """Show image of ship"""
+
         self.screen.blit(self.image, self.rect)
 
         for life in self.lifes:
             life.blitme()
 
     def update(self):
-        if self.movingL and self.rect.centerx >= 35:
+        """Update ship position"""
+
+        if self.moving_left and self.rect.centerx >= 35:
             self.rect.centerx -= self.velocity
 
             for life in self.lifes:
                 life.rect.centerx -= self.velocity
 
-        if self.movingR and self.rect.centerx <= 1165:
+        if self.moving_right and self.rect.centerx <= 1165:
             self.rect.centerx += self.velocity
 
             for life in self.lifes:
                 life.rect.centerx += self.velocity
 
     def hit(self):
+        """Verify hit in enemy"""
+
         self.life -= 1
 
         if self.life % 2 == 0:
