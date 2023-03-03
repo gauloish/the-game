3c11942e62a1e4a6432d0345422734406be0e46b
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-02 22:03:35 -0300

    added a documentation to classes and methods in settings.py

--- program/settings.py
+++ program/settings.py
@@ -1,7 +1,24 @@
+""" Settings
+
+General settings to Game
+"""
+
+
 class Settings:
+    """Settings to Game
+
+    Attributes:
+        width: int
+        height: int
+        back: list[int]
+        display: str
+    """
+
     def __init__(self):
+        """Initialize Settings"""
+
         self.width = 1200
-        self.height = 800
+        self.height = 80
 
         self.back = [230, 230, 230]
 
