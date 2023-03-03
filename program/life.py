aaf59a1821f9f9429608805f801c614e97b7a0ec
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-02 21:42:06 -0300

    added a documentation to classes and methods in life.py

--- program/life.py
+++ program/life.py
@@ -1,10 +1,33 @@
+""" Life
+
+Class Life to emulate life to entities with a sprint
+"""
+
 import pygame
 
 from .functions import getPath
 
 
 class Life:
-    def __init__(self, screen, x, y):
+    """Life To Entities
+
+    Attributes:
+        screen: pygame.Surface
+        image: pygame.Surface
+        rect: pygame.Rect
+        screen_rect: pygame.Rect
+        centerx: int
+        bottom: int
+    """
+
+    def __init__(self, screen, center, bottom):
+        """Initialize Bullet
+
+        Args:
+            screen (pygame.Surface): game screen
+            center (int): horizontal center position of image
+            bottom (int): vertical bottom position of image
+        """
         self.screen = screen
 
         self.image = pygame.image.load(getPath("images/life/life.png"))
@@ -12,8 +35,9 @@ class Life:
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
-        self.rect.centerx = x
-        self.rect.bottom = y
+        self.rect.centerx = center
+        self.rect.bottom = bottom
 
     def blitme(self):
+        """Show Image of Life"""
         self.screen.blit(self.image, self.rect)
