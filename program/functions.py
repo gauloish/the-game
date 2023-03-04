38a506194ad8d1245055716d75d2406ab58e3c81
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-04 00:31:36 -0300

    removed ´getPath()´ from program/functions.py

--- program/functions.py
+++ program/functions.py
@@ -1,31 +1,10 @@
 import sys
 import pygame
-from os.path import join
-from pathlib import Path
 
 from .settings import Settings
 from .canvas import Background, Initial, Options, Ships, Level, Controls, Count, End
 from .ship import Ship
 from .enemies import Pawn, Knight, Bishop
-
-####################################processing###################################
-
-
-def getPath(path: str) -> str:
-    """Get and resolve path
-
-    Args:
-        path (str): image path
-
-    Returns:
-        path (str): path resolved
-    """
-
-    if sys.platform in ["win32", "cygwin", "msys"]:
-        path.replace("/", "\\")
-
-    return join(Path.cwd(), path)
-
 
 ######################################Start######################################
 
