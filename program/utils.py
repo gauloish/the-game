dc594b4a32efbf84411476ae389305be3d109319
matheus moreno <o.matheus.moreno@gmail.com>
2023-03-04 00:33:28 -0300

    moved ´getPath()´ from program/functions.py to program/utils.py to avoid ciclic imports

---
+++ program/utils.py
@@ -0,0 +1,20 @@
+import sys
+
+from os.path import join
+from pathlib import Path
+
+
+def getPath(path: str) -> str:
+    """Get and resolve path
+
+    Args:
+        path (str): image path
+
+    Returns:
+        path (str): path resolved
+    """
+
+    if sys.platform in ["win32", "cygwin", "msys"]:
+        path.replace("/", "\\")
+
+    return join(Path.cwd(), path)
